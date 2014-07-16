1db2a1906b1bd905dfbf5b397f0f64551ec15c8d
Martin Görner <martin.gorner@gmail.com>
2014-07-17 00:01:57 +0200

    Implemented missing functionalities: datastore persistency, polymer repeat tempting and the close button

--- src/com/endpoints/tutorial/Greeting.java
+++ src/com/endpoints/tutorial/Greeting.java
@@ -1,7 +1,14 @@
 package com.endpoints.tutorial;
 
+import com.googlecode.objectify.annotation.Entity;
+import com.googlecode.objectify.annotation.Id;
+import com.googlecode.objectify.annotation.Index;
+
+@Entity
 public class Greeting {
+	@Id public Long id;
+	public Greeting() {}
 	public Greeting(String text, Integer avatarId) { this.text = text; this.avatarId = avatarId; }
-	public String  text;
-	public Integer avatarId;
+	@Index public String  text;
+	@Index public Integer avatarId;
 }
--- src/com/endpoints/tutorial/GreetingsAPI.java
+++ src/com/endpoints/tutorial/GreetingsAPI.java
@@ -3,9 +3,12 @@ package com.endpoints.tutorial;
 import java.util.Date;
 import java.util.Random;
 
+import static com.endpoints.tutorial.ObjectifyDAO.ofy; 
+
 import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
 import com.google.api.server.spi.config.Named;
+import com.googlecode.objectify.Key;
 
 @Api(
 	    name = "greetings",  /* mandatory, otherwise replaced with "myapi"*/
@@ -18,7 +21,13 @@ public class GreetingsAPI
 	public Greeting hello(@Named("who") String who)
 	{
 		String text = new String("Hello ").concat(who);
-		Greeting g = new Greeting(text, new Random(new Date().getTime()).nextInt(16));
+		Greeting g = ofy().load().type(Greeting.class).filter("text", text).first().now();
+		if (g == null)
+		{
+			g = new Greeting(text, new Random(new Date().getTime()).nextInt(16));
+			Key<Greeting> gkey = ofy().save().entity(g).now();
+			g.id = gkey.getId();
+		}
 		return g;
 	}
 }
